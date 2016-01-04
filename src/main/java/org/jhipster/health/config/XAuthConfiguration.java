2a0e3c460d3aa0a414723b647cda7814f0924bde
Zhen Kyle <zhenkyle@gmail.com>
2016-01-04 23:00:16 +0800

    Initial checkin of 21-points application

---
+++ src/main/java/org/jhipster/health/config/XAuthConfiguration.java
@@ -0,0 +1,19 @@
+package org.jhipster.health.config;
+
+import org.jhipster.health.security.xauth.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+* Configures x-auth-token security.
+*/
+@Configuration
+public class XAuthConfiguration {
+
+    @Bean
+    public TokenProvider tokenProvider(JHipsterProperties jHipsterProperties) {
+        String secret = jHipsterProperties.getSecurity().getAuthentication().getXauth().getSecret();
+        int validityInSeconds = jHipsterProperties.getSecurity().getAuthentication().getXauth().getTokenValidityInSeconds();
+        return new TokenProvider(secret, validityInSeconds);
+    }
+}
